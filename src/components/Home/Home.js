8c6d03f545ebb8ff4944219f2fa6df5fae0801dd
shahzad <munirshahzad044@gmail.com>
2022-03-27 16:59:15 +0500

    new branch added

---
+++ src/components/Home/Home.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Distinctio, velit iure. Consequuntur vitae eos reprehenderit quibusdam totam esse sed id aliquid optio magnam? Unde qui ipsum eveniet magnam saepe fugit iure rerum, illo pariatur.
+    </div>
+  )
+}
+
+export default Home
